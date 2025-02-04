979bbe356922eb30a20e7f8248f1fb814013796c
Mitra Zakeri <mitrazkri79@gmail.com>
2025-02-04 23:59:48 +0330

    add que

--- ESP32-Foo-Bar.ino
+++ ESP32-Foo-Bar.ino
@@ -1,60 +1,111 @@
 #include <Arduino.h>
 
-String type = "";
-int n;
 
-bool is_prime(int number){
-    if(number < 2) return false;
-    if(number == 2) return true;
-    if(number % 2 == 0) return false;
-    for(int i=3; (i*i)<=number; i+=2){
-        if(number % i == 0 ) return false;
+#define MAX_BUFFER 8
+
+QueueHandle_t queueEven, queueOdd;
+SemaphoreHandle_t timingSemaphore;
+
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
     }
     return true;
 }
 
-void even_func(void *pvParameters) {
-    int i = (int) pvParameters; 
-    type += "Foo";
-    Serial.printf("\nFoo %d\n", i);
-    vTaskDelete(NULL);
+String createLabel(int num) {
+    return (num % 2 ? "Bar" : "Foo") + String(" ") + String(num) + (isPrime(num) ? " Prime" : "");
 }
 
-void odd_func(void *pvParameters) {
-    int i = (int) pvParameters;
-    type += "Bar";
-    Serial.printf("\nBar %d\n", i);
-    vTaskDelete(NULL);
+// if true, core 1 is selected, else core 0
+bool selectCore(int num) {
+  bool core = (num % 2) ? 1 : 0;
+  return (isPrime(num)) ? 1 : core;
 }
 
-void prime_func(void *pvParameters) {
-    int i = (int) pvParameters;
-    type += "Prime";
-    Serial.printf("\nPrime %d\n", i);
-    vTaskDelete(NULL);
+void outputMessage(const char* label) {
+    xSemaphoreTake(timingSemaphore, portMAX_DELAY);
+    Serial.println(label);    
+    xSemaphoreGive(timingSemaphore);
 }
 
+void taskEven(void *parameter) {
+    int num;
+    while (true) {
+        if (xQueueReceive(queueEven, &num, portMAX_DELAY) == pdPASS) {
+            outputMessage(createLabel(num).c_str());
+        }
+    }
+}
 
+void taskOdd(void *parameter) {
+    int num;
+    while (true) {
+        if (xQueueReceive(queueOdd, &num, portMAX_DELAY) == pdPASS) {
+            outputMessage(createLabel(num).c_str());
+        }
+    }
+}
+
+int readInput() {
+    while (Serial.available()) {}
+    String input = Serial.readStringUntil('\n');
+    int n = input.toInt();
+    Serial.printf("Received %d\n", n);
+    return n;
+}
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("enter");
-  while (Serial.available() == 0) {
-  }
-  String input = Serial.readStringUntil('\n');
-  int n = input.toInt();
-  Serial.print("Received ");
-  Serial.println(n);
+    Serial.begin(9600);
+    while (!Serial);
 
-  for (int i=n; i>=0; i--){
-    if ((i%2)==0) {
-      xTaskCreatePinnedToCore(even_func, "even_task", 10000, (void*) i, 1, NULL, 0);}
-    if ((i%2)==1){
-      xTaskCreatePinnedToCore(odd_func, "odd_task", 10000, (void*) i, 1, NULL, 1);}
-    if (is_prime(i)){
-      xTaskCreatePinnedToCore(prime_func, "prime_task", 10000, (void*) i, 1, NULL, 0);}
-  }
+    queueEven = xQueueCreate(MAX_BUFFER, sizeof(int));
+    queueOdd = xQueueCreate(MAX_BUFFER, sizeof(int));
+    timingSemaphore = xSemaphoreCreateBinary();
+    xSemaphoreGive(timingSemaphore);
+
+    xTaskCreatePinnedToCore(taskEven, "TaskEven", 1000, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(taskOdd, "TaskOdd", 1000, NULL, 1, NULL, 1);
+
+    Serial.println("Enter a positive integer:");
+    while (Serial.available()) {}
+    String input = Serial.readStringUntil('\n');
+    int n = input.toInt();
+    Serial.printf("Received %d\n", n);
+
+    for (int i = n; i >= 0; i--) {
+        delay(1000);
+        if (selectCore(i) == 0) {
+            xQueueSend(queueEven, &i, portMAX_DELAY); // even on core 0
+        } else {
+            xQueueSend(queueOdd, &i, portMAX_DELAY); // odd on core 1
+        }
+    }
 }
 
 void loop() {
+    if (Serial.available()) {
+        String input = Serial.readStringUntil('\n');
+        int n = input.toInt();
+        Serial.printf("Received %d\n", n);
+
+        if (n == 0) {
+            Serial.println("Restarting ESP32...");
+            ESP.restart();
+        } 
+        else if (uxQueueMessagesWaiting(queueEven) + uxQueueMessagesWaiting(queueOdd) < MAX_BUFFER) {
+            for (int i = n; i >= 0; i--) {
+                delay(1000);
+                if (selectCore(i) == 0) {
+                    xQueueSend(queueEven, &i, portMAX_DELAY);
+                } else {
+                    xQueueSend(queueOdd, &i, portMAX_DELAY);
+                }
+            }
+        } 
+        else {
+            Serial.println("Buffer is full");
+        }
+    }
 }
